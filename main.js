56d210b6eafa9070593f211a07ca7e5355b26e7f
Ivan Stojadinovic <stojadinovicivan0@gmail.com>
2025-01-14 18:21:51 +0100

    displayBooks fn done, needs some finishing up

--- main.js
+++ main.js
@@ -7,6 +7,21 @@ const bookForm = document.querySelector("#book-form");
 
 let bookList = [];
 
+const displayBooks = () => {
+    let booksHTML = "";
+    booksHTML += bookList.map((book) => {
+        return `
+            <div class="book">
+                <h1 class="name">${book.name} (${book.year})</h1>
+                <h3 class="writer">${book.writer}</h3>
+                <p class="isbn">${book.isbn}</p>
+                <button>Rent Now!</button>
+            </div>
+        `
+    }).join('');
+    libraryContainer.innerHTML = booksHTML;
+}
+
 openDialogButton.addEventListener("click", () => {
     dialog.showModal();
 });
@@ -14,3 +29,17 @@ openDialogButton.addEventListener("click", () => {
 closeDialogButton.addEventListener("click", () => {
     dialog.close();
 });
+
+addBookButton.addEventListener("click", () => {
+    let book = {
+        name: bookForm.name.value,
+        writer: bookForm.writer.value,
+        isbn: bookForm.isbn.value,
+        year: bookForm.year.value,
+        condition: bookForm.condition.value,
+        cover: bookForm.cover.value
+    }
+    bookList.push(book);
+    displayBooks();
+})
+
